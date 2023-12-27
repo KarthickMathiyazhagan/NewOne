c58c044a72e8c1bee4ff7bb5660ac4694d8f6cd3
Karthick M <137099994+KarthickMathiyazhagan@users.noreply.github.com>
2023-12-27 08:20:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+let num=13234343434;
+console.log(num*3636);
+console.log(typeof num)
+console.log(Number.MAX_VALUE * 10)
+
+let fisrt="kar\tthi\nck\"M\""
+let name="M"
+console.log(fisrt+name);
+
+let bool=4>6
+console.log(bool)
+
+let user=null
+console.log(user)
+
+
